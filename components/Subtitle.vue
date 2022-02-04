ac74e8d4f75b938c065458c0055b0c1d90e8d3b3
Diego M. Rodrigues <diegomr86@gmail.com>
2022-02-04 15:05:36 -0300

    Personaliza o Pri Responde

---
+++ components/Subtitle.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="d-flex align-center justify-center">
+    <v-btn left fixed :to="backTo" icon outlined color="primary" class="ml-1">
+      <v-icon>mdi-chevron-left</v-icon>
+    </v-btn>
+    <h2 class="text-h5" style="color: rgba(123, 163, 162, 0.8)">
+      {{ label }}
+    </h2>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    backTo: {
+      type: String,
+      default: '/dashboard',
+    },
+  },
+}
+</script>
