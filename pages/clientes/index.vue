cbd0022360c2abb71094a5fbc92c4cd81a16b27b
Diego M. Rodrigues <diegomr86@gmail.com>
2022-05-10 20:29:21 -0300

    adiciona busca

--- pages/clientes/index.vue
+++ pages/clientes/index.vue
@@ -5,7 +5,14 @@
       <div class="mb-8">
         <Subtitle label="Todas as clientes" />
       </div>
-
+      <v-text-field
+        v-model="search"
+        type="search"
+        clearable
+        outlined
+        label="Buscar cliente"
+        append-icon="mdi-magnify"
+      />
       <DoubleTable :data="membersDataset" clickable />
     </v-container>
     <div v-else class="text-center py-6">
@@ -14,21 +21,35 @@
   </div>
 </template>
 <script>
+import slugify from 'slugify'
 export default {
   data() {
     return {
       members: null,
+      search: '',
     }
   },
   computed: {
     membersDataset() {
       if (this.members) {
+        let filteredMembers = this.members
+
+        if (this.search) {
+          filteredMembers = filteredMembers.filter((member) => {
+            return (
+              member.name &&
+              slugify(member.name.toLowerCase()).includes(
+                slugify(this.search.toLowerCase())
+              )
+            )
+          })
+        }
         return {
           header: {
             label: 'Adesão',
             value: 'Cliente',
           },
-          items: this.members.map((member) => ({
+          items: filteredMembers.map((member) => ({
             label: this.$moment(member.createdAt).format('DD/MM/YYYY'),
             value: member.name || member.email,
             url: `/clientes/${member._id}`,
