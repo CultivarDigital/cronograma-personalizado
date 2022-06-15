2f72175ecbb91bda1d7fa43980a06202ba52fdda
Diego M. Rodrigues <diegomr86@gmail.com>
2022-06-15 16:59:28 -0300

    Finaliza minicurso

--- pages/mini-curso/index.vue
+++ pages/mini-curso/index.vue
@@ -1,33 +1,38 @@
+<!-- eslint-disable vue/no-v-html -->
 <template>
   <div>
     <TopNavigation active="Mini curso" />
-    <div class="text-center">
-      <img
-        class="mt-n6"
-        src="~/assets/img/mini-curso.png"
-        alt="Mini curso"
-        style="max-width: 100%"
-      />
+    <div v-if="playVideo" class="text-center">
+      <div class="video-player">
+        <iframe
+          :title="playVideo.name"
+          :src="playVideo.player_embed_url"
+          allowfullscreen
+          frameborder="0"
+        ></iframe>
+      </div>
     </div>
     <v-list>
-      <template v-for="(video, i) in playlist">
-        <v-list-item :key="i" class="primary--text">
+      <template v-for="(video, i) in videos">
+        <v-list-item :key="i" class="primary--text" @click="playVideo = video">
           <v-list-item-content>
             <div class="px-6">
-              <p class="mb-1">{{ i }}. {{ video.title }}</p>
+              <p class="mb-1">{{ i }}. {{ video.name }}</p>
               <p
                 class="font-weight-black mb-0"
                 style="color: rgba(123, 163, 162, 0.6)"
               >
-                {{ video.duration }}
+                {{ toTimeString(video.duration) }}
               </p>
             </div>
           </v-list-item-content>
           <v-list-item-action>
-            <img
-              :src="require('~/assets/img/' + video.status + '.png')"
-              :alt="video.status"
-            />
+            <v-icon
+              v-if="playVideo && playVideo.uri === video.uri"
+              color="primary"
+              >mdi-play-circle</v-icon
+            >
+            <v-icon v-else color="primary">mdi-play-circle-outline</v-icon>
           </v-list-item-action>
         </v-list-item>
         <v-divider :key="'divider-' + i" class="my-2" />
@@ -39,35 +44,35 @@
 export default {
   data() {
     return {
-      tab: 4,
-      playlist: [
-        {
-          title: 'Como lavar o cabelo',
-          duration: '10:59',
-          status: 'played',
-        },
-        {
-          title: 'Como fazer umectação',
-          duration: '4:25',
-          status: 'waiting',
-        },
-        {
-          title: 'Aplicando máscara corretamente',
-          duration: '5:40',
-          status: 'playing',
-        },
-        {
-          title: 'Como aplicar o leave-in',
-          duration: '8:47',
-          status: 'waiting',
-        },
-      ],
+      videos: null,
+      playVideo: null,
     }
+  },
+  async created() {
+    this.videos = await this.$axios.$get('/v1/videos')
+    if (this.videos && this.videos.length) {
+      this.playVideo = this.videos[0]
+    }
+  },
+  methods: {
+    toTimeString(seconds) {
+      if (seconds) {
+        let formated = new Date(seconds * 1000)
+          .toUTCString()
+          .match(/(\d\d:\d\d:\d\d)/)[0]
+
+        if (formated.startsWith('00:')) {
+          formated = formated.substring(3)
+        }
+        return formated
+      }
+    },
   },
 }
 </script>
 <style lang="scss">
-.v-slide-group__prev {
-  display: none !important;
+.video-player iframe {
+  width: 100%;
+  height: calc(55vw);
 }
 </style>
